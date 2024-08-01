fa1d6f7ea8a2b138a16c9a0a5e498571858efdf2
james2000-design <gbakonjames88@gmail.com>
2024-08-01 17:00:49 +0100

    completed and fixed bugs

---
+++ components/MealDetail/Subtitle.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+interface subtitleProps {
+  children: ReactNode;
+}
+
+function Subtitle({ children }: subtitleProps) {
+  return (
+    <View style={styles.subtitleContainer}>
+      <Text style={styles.subtitle}>{children}</Text>
+    </View>
+  );
+}
+
+export default Subtitle;
+
+const styles = StyleSheet.create({
+  subtitle: {
+    color: "#e2b497",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  subtitleContainer: {
+    padding: 6,
+    marginHorizontal: 12,
+    marginVertical: 4,
+    borderBottomColor: "#e2b497",
+    borderBottomWidth: 2,
+  },
+});
---
+++ models/category.ts
@@ -0,0 +1,10 @@
+class Category {
+  [x: string]: string;
+  constructor(id: string, title: string, color: string) {
+    this.id = id;
+    this.title = title;
+    this.color = color;
+  }
+}
+
+export default Category;
